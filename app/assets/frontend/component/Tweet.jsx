a660605ff2d63ad5dbdb25474589de64b509e475
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-07-27 04:58:29 +0900

    mockup

---
+++ app/assets/frontend/component/Tweet.jsx
@@ -0,0 +1,11 @@
+export default class Tweet extends React.Component {
+  render() {
+    return (
+      <li className="collection-item avatar">
+        <i className="material-icons circle">일등</i>
+        <span className="title">내용</span>
+        <p>First Line</p>
+      </li>
+    );
+  }
+}
--- app/assets/frontend/component/TweetList.jsx
+++ app/assets/frontend/component/TweetList.jsx
@@ -1,18 +1,11 @@
+import Tweet from "./Tweet"
+
 export default class TweetList extends React.Component {
   render() {
     return (
       <div>
         <ul className ="collection">
-          <li className="collection-item avatar">
-            <i className="material-icons circle">일등</i>
-            <span className="title">내용</span>
-            <p>First Line</p>
-          </li>
-          <li className="collection-item avatar">
-            <i className="material-icons circle">이둥</i>
-            <span className="title">내용</span>
-            <p>First Line</p>
-          </li>
+          <Tweet />
         </ul>
       </div>
     );
