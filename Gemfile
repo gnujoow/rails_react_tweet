a694ffed713366584c13f561ff33b5b1bdeb61de
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-07-27 03:01:27 +0900

    react Component added
     - react component added
     - SEO by prerendering

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+#for using reactjs
+gem 'react-rails'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,17 @@
+class Main extends React.Component {
+  render() {
+    return(
+      <h1>hello react!</h1>
+    );
+  }
+}
+
+
+// let documentReady = () =>{
+//   ReactDOM.render(
+//       <Main />,
+//       document.getElementById('main')
+//   );
+// };
+//
+// $(documentReady);
