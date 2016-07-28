bf78577c0c7d819aa3315c3b5695d9d9bd38a945
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-07-28 17:21:14 +0900

    react gem to react npm package

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,66 +1,12 @@
-import TweetBox from "./component/TweetBox";
-import TweetList from "./component/TweetList";
-import TweetStore from "./stores/TweetStore";
-
-import TweetActions from "./actions/TweetActions";
-TweetActions.getAllTweets();
-
-let getAppState = () => {
-  return { tweetsList: TweetStore.getAll() };
-}
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = getAppState();
-    this._onChange = this._onChange.bind(this);
-  }
-  // formattedTweets(tweetsList){
-  //   let formattedList = tweetsList.map(tweet => {
-  //     tweet.formattedDate = moment(tweet.created_at).fromNow();
-  //     return tweet;
-  //   });
-  //   return {
-  //     tweetsList: formattedList
-  //   };
-  // }
-  componentDidMount() {
-    TweetStore.addChangeListener(this._onChange);
-    // $.ajax("/tweets")
-    // .success(data => this.setState(this.formattedTweets(data)))
-    // .error(error => console.log(error));
-  }
-  componentWillUnMount() {
-    TweetStore.removeChangeListener(this._onChange);
-  }
-  _onChange() {
-    console.log(5, "Main._onChange");
-    this.setState(getAppState());
-  }
-  // addTweet(tweetToAddd){
-  //   $.post("/tweets", { body: tweetToAddd})
-  //   .success( savedTweet => {
-  //     let newTweetsList = this.state.tweetsList;
-  //     newTweetsList.unshift(savedTweet);
-  //     this.setState(this.formattedTweets(newTweetsList));
-  //   })
-  //   .error(error => console.log(error));
-  // }
-  render() {
-    return(
-      <div className ="container">
-        <TweetBox />
-        <TweetList tweets={this.state.tweetsList} />
-      </div>
-    );
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './component/Index';
 
 let documentReady = () =>{
   let reactNode = document.getElementById('main');
   if(reactNode) {
     ReactDOM.render(
-        <Main />,
+        <Index />,
         document.getElementById('main')
     );
   }
